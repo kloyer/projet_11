887e6f828caf2438afc5e0d84f2fea60dc962427
kloyer <k.loyer07@gmail.com>
2023-11-24 16:08:25 +0100

    fontawesome for stars and fix responsive related

--- src/components/Rating/Rating.jsx
+++ src/components/Rating/Rating.jsx
@@ -1,25 +1,26 @@
 import React from 'react';
 import './Rating.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar as solidStar } from '@fortawesome/free-solid-svg-icons';
+import { faStar as regularStar } from '@fortawesome/free-regular-svg-icons';
 
 // Rating functional component
 function Rating({ rating }) {
     // Creating an array of 5 elements to represent stars
     const ratingStars = Array.from({ length: 5 }, (_, index) => {
-      // Filling the star ('★') if the index is less than the rating value, otherwise empty star ('☆')
-      return index < rating ? '★' : '☆';
+        // Using solid star for filled, regular star for empty
+        return index < rating ? solidStar : regularStar;
     });
-  
+
     // Returning the JSX for the star rating
     return (
-      <div className="star-rating">
-        {/* Mapping over the ratingStars array to render each star */}
-        {ratingStars.map((star, index) => (
-          // Using a span to display each star. Applying a class based on whether the star is filled or empty
-          <span key={index} className={star === '★' ? 'filled-star' : 'empty-star'}>
-            {star}
-          </span>
-        ))}
-      </div>
+        <div className="star-rating">
+            {/* Mapping over the ratingStars array to render each star */}
+            {ratingStars.map((star, index) => (
+                // Using FontAwesomeIcon to display each star
+                <FontAwesomeIcon key={index} icon={star} className={star === solidStar ? 'filled-star' : 'empty-star'} />
+            ))}
+        </div>
     );
 }
 
